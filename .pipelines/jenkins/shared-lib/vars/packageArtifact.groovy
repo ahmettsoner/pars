89ee323b8dbfac7719cd4d2313f0f417d655b8ce
Your Name <you@example.com>
2025-05-13 13:54:35 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/packageArtifact.groovy
+++ .pipelines/jenkins/shared-lib/vars/packageArtifact.groovy
@@ -49,7 +49,7 @@ def call(String OS, String ARCH, String archiveFormat) {
     sh "cp '${binaryTempPath}/${newBaseName}' '${env.ARTIFACT_PATH}/${newBaseName}'"
 
     def checksum = sh(script: "sha256sum '${binaryTempPath}/${newBaseName}' | awk '{print \$1}'", returnStdout: true).trim()
-    echo "Checksum for ${binaryTempPath}/${newBaseName}: ${archiveChecksum}"
+    echo "Checksum for ${binaryTempPath}/${newBaseName}: ${checksum}"
 
     def type = "Archive"
     def line = "| ${OS} | ${ARCH} | ${type} | ${newBaseName} | ${checksum} |"
