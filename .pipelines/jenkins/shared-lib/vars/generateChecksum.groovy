f1389c19c8f5540fdc04919b3d61870b931868b8
Your Name <you@example.com>
2025-05-13 08:18:42 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
+++ .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
@@ -1,30 +1,28 @@
 def generateChecksum(String label) {
-    node(label) {
-        stage("Create ${label.toUpperCase()} markdown file for checksums") {
-            steps {
-                script {
-                    def versionOutput = readFile("${WORKSPACE}/version_output.txt")
-                    def buildVersion = versionOutput.split('\n').find { it.startsWith('BUILD_VERSION=') }?.split('=')[1]?.trim()
+      stage("Create ${label.toUpperCase()} markdown file for checksums") {
+          steps {
+              script {
+                  def versionOutput = readFile("${WORKSPACE}/version_output.txt")
+                  def buildVersion = versionOutput.split('\n').find { it.startsWith('BUILD_VERSION=') }?.split('=')[1]?.trim()
 
-                    echo "Build Version: ${buildVersion}"
+                  echo "Build Version: ${buildVersion}"
 
-                    if (!buildVersion) {
-                        error "BUILD_VERSION is empty or null"
-                    }
+                  if (!buildVersion) {
+                      error "BUILD_VERSION is empty or null"
+                  }
 
-                    def ARTIFACT_PATH = "dist/artifacts/${buildVersion}"
+                  def ARTIFACT_PATH = "dist/artifacts/${buildVersion}"
 
-                    sh """
-                        echo "Creating directory: ${ARTIFACT_PATH}"
-                        mkdir -p "${ARTIFACT_PATH}"
+                  sh """
+                      echo "Creating directory: ${ARTIFACT_PATH}"
+                      mkdir -p "${ARTIFACT_PATH}"
 
-                        echo "| Platform | Architecture | Type      | File Name | SHA-256 Checksum |" > ${ARTIFACT_PATH}/Checksums.md
-                        echo "|----------|--------------|-----------|-----------|------------------|" >> ${ARTIFACT_PATH}/Checksums.md
-                    """
-                }
-            }
-        }
-    }
+                      echo "| Platform | Architecture | Type      | File Name | SHA-256 Checksum |" > ${ARTIFACT_PATH}/Checksums.md
+                      echo "|----------|--------------|-----------|-----------|------------------|" >> ${ARTIFACT_PATH}/Checksums.md
+                  """
+              }
+          }
+      }
 }
 
 return this
