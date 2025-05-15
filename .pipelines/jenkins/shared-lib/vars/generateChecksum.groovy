3d4dd96ffc1ad779b53c9ca1fa2b534d92016d1b
Your Name <you@example.com>
2025-05-15 12:48:14 +0300

    el

--- .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
+++ .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
@@ -1,17 +1,4 @@
 def call(String OS, String ARCH) {
-    echo "Build Version: ${env.BUILD_VERSION}"
-
-    env.ARTIFACT_CHECKSUM_MD5_PATH = "${env.ARTIFACT_PATH}/Checksums.md"
-    if (!env.BUILD_VERSION) {
-        error "BUILD_VERSION is empty or null"
-    }
-
-    sh """
-        echo "| Platform | Architecture | Type      | File Name | SHA-256 Checksum |" > ${env.ARTIFACT_CHECKSUM_MD5_PATH}
-        echo "|----------|--------------|-----------|-----------|------------------|" >> ${env.ARTIFACT_CHECKSUM_MD5_PATH}
-    """
-
-
     def ext = ""
     if (OS.toLowerCase() == 'windows') {
         ext = ".exe"
@@ -22,7 +9,7 @@ def call(String OS, String ARCH) {
 
     def type = "Binary"
     def line = "| ${OS} | ${ARCH} | ${type} | ${newBaseName} | ${checksum} |"
-    sh "echo '${line}' >> ${env.ARTIFACT_PATH}/${newBaseName}-checksum.txt"
+    sh "echo '${line}' >> ${env.ARTIFACT_CHECKSUM_MD5_PATH}"
 
     archiveArtifacts artifacts: 'dist/artifacts/**/*', fingerprint: true
 }
---
+++ .pipelines/jenkins/shared-lib/vars/generateChecksumFile.groovy
@@ -0,0 +1,16 @@
+def call(String OS, String ARCH) {
+    echo "Build Version: ${env.BUILD_VERSION}"
+
+    env.ARTIFACT_CHECKSUM_MD5_PATH = "${env.ARTIFACT_PATH}/Checksums.md"
+    if (!env.BUILD_VERSION) {
+        error "BUILD_VERSION is empty or null"
+    }
+
+    sh """
+        echo "| Platform | Architecture | Type      | File Name | SHA-256 Checksum |" > ${env.ARTIFACT_CHECKSUM_MD5_PATH}
+        echo "|----------|--------------|-----------|-----------|------------------|" >> ${env.ARTIFACT_CHECKSUM_MD5_PATH}
+    """
+    archiveArtifacts artifacts: 'dist/artifacts/**/*', fingerprint: true
+}
+
+return this
