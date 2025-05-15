342f32c9a761652426fe85a8df55e7a7f57f7a4b
Your Name <you@example.com>
2025-05-15 12:44:38 +0300

    el

--- .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
+++ .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String OS, String ARCH) {
     echo "Build Version: ${env.BUILD_VERSION}"
 
     env.ARTIFACT_CHECKSUM_MD5_PATH = "${env.ARTIFACT_PATH}/Checksums.md"
@@ -10,6 +10,21 @@ def call() {
         echo "| Platform | Architecture | Type      | File Name | SHA-256 Checksum |" > ${env.ARTIFACT_CHECKSUM_MD5_PATH}
         echo "|----------|--------------|-----------|-----------|------------------|" >> ${env.ARTIFACT_CHECKSUM_MD5_PATH}
     """
+
+
+    def ext = ""
+    if (OS.toLowerCase() == 'windows') {
+        ext = ".exe"
+    }
+
+    def newBaseName = "${APPNAME}-${OS}-${ARCH}${ext}"
+    def checksum = sh(script: "sha256sum ${env.ARTIFACT_PATH}/${newBaseName} | awk '{print \$1}'", returnStdout: true).trim()
+
+    def type = "Binary"
+    def line = "| ${OS} | ${ARCH} | ${type} | ${newBaseName} | ${checksum} |"
+    sh "echo '${line}' >> ${env.ARTIFACT_PATH}/${newBaseName}-checksum.txt"
+
+    archiveArtifacts artifacts: 'dist/artifacts/**/*', fingerprint: true
 }
 
 return this
