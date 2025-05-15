40850661888b05c2c1fa8cff817860d754a65572
Your Name <you@example.com>
2025-05-15 16:37:49 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
+++ .pipelines/jenkins/shared-lib/vars/generateChecksum.groovy
@@ -1,15 +1,11 @@
-def call(String OS, String ARCH) {
-    def ext = ""
-    if (OS.toLowerCase() == 'windows') {
-        ext = ".exe"
-    }
-
+def WriteChecksumeForBinary(String OS, String ARCH) {
+    def ext = OS.toLowerCase() == 'windows' ? '.exe' : ''
     def newBaseName = "${APPNAME}-${OS}-${ARCH}${ext}"
     def checksum = sh(script: "sha256sum ${env.ARTIFACT_PATH}/${newBaseName} | awk '{print \$1}'", returnStdout: true).trim()
-
     def type = "Binary"
     def line = "| ${OS} | ${ARCH} | ${type} | ${newBaseName} | ${checksum} |"
-    sh "echo '${line}' >> ${env.ARTIFACT_CHECKSUM_MD5_PATH}"
-}
 
-return this
+    // Write to a temporary file
+    writeFile file: "checksum-${OS}-${ARCH}.txt", text: "${line}\n"
+    stash includes: "checksum-${OS}-${ARCH}.txt", name: "checksum-${OS}-${ARCH}"
+}
