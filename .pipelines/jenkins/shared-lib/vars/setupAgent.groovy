c187428fd4d2a8bf544d4546ba778f1ff3a94e37
Your Name <you@example.com>
2025-05-16 12:58:22 +0300

    elk

--- .pipelines/jenkins/shared-lib/vars/setupAgent.groovy
+++ .pipelines/jenkins/shared-lib/vars/setupAgent.groovy
@@ -1,7 +1,6 @@
 def call(String BRANCH) {
   checkout scm
-  sh "git config --global --add safe.directory '${env.WORKSPACE}'"
-
+  sh "git -C '${env.WORKSPACE}' config --global --add safe.directory '${env.WORKSPACE}'"
 
   sh """
     git config --global user.email "${GIT_USER_EMAIL}"
