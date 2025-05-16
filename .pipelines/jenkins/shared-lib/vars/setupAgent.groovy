2dddb266aab3720506317988fccee7656e3274e2
Your Name <you@example.com>
2025-05-16 12:51:59 +0300

    elk

--- .pipelines/jenkins/shared-lib/vars/setupAgent.groovy
+++ .pipelines/jenkins/shared-lib/vars/setupAgent.groovy
@@ -44,7 +44,6 @@ def call(String BRANCH) {
   echo "Creating directory: ${env.ARTIFACT_PATH}"
   sh "mkdir -p '${env.CHANGES_PATH}'"
   sh "mkdir -p '${env.ARTIFACT_PATH}'"
-  echo " >>>> 1"
 
   sh "make build.cmake.${env.BUILD_VERSION}"
 }
