9864dce4ca573b2c976eb53c7fb3929793f6f152
Your Name <you@example.com>
2025-05-27 17:01:44 +0300

    ek1

--- .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
@@ -9,13 +9,15 @@ def call(String OS, String ARCH) {
     sh "echo \"${GPG_PRIVATE_KEY_B64}\" | base64 -d > /tmp/private.gpg"
     sh "gpg --batch --import /tmp/private.gpg"
 
-    sh "KEY_FPR=\"$(gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10)\""
-    sh "echo -e \"5\ny\n\" | gpg --command-fd 0 --expert --edit-key \"$KEY_FPR\" trust"
-    sh "echo \"use-agent\" >> ~/.gnupg/gpg.conf"
-    sh "echo \"allow-loopback-pinentry\" >> ~/.gnupg/gpg-agent.conf"
-    sh "gpgconf --kill gpg-agent"
-    sh "GPG_TTY=$(tty)"
-    sh "export GPG_TTY"
+    def keyFpr = sh(script: "gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10", returnStdout: true).trim()
+
+    sh """
+    echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key "$keyFpr" trust quit
+    echo "use-agent" >> ~/.gnupg/gpg.conf
+    echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+    gpgconf --kill gpg-agent
+    export GPG_TTY=$(tty)
+    """
 
     // sh "curl -v -u \"$NEXUS_USER:$NEXUS_PASS\" --upload-file ~/rpmbuild/RPMS/x86_64/my-package.rpm \"$NEXUS_URL/my-package.rpm\""
 
