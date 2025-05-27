f09f023cf114f6845ccecef12ecf89d0e6084c57
Your Name <you@example.com>
2025-05-27 17:16:28 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
@@ -4,6 +4,7 @@ def call(String OS, String ARCH) {
     def rpmOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
 
 
+    sh "curl -v -u \"$NEXUS_USER:$NEXUS_PASS\" --upload-file ~/rpmbuild/RPMS/x86_64/my-package.rpm \"$NEXUS_URL/my-package.rpm\""
 
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
