289f3eb500d9048c72e0705758bb2dbbb3f49476
Your Name <you@example.com>
2025-05-27 17:04:22 +0300

    ek1

--- .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
@@ -9,15 +9,15 @@ def call(String OS, String ARCH) {
     sh "echo \"${GPG_PRIVATE_KEY_B64}\" | base64 -d > /tmp/private.gpg"
     sh "gpg --batch --import /tmp/private.gpg"
 
-    def keyFpr = sh(script: "gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10", returnStdout: true).trim()
-
-    sh """
-    echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key "$keyFpr" trust quit
+    sh '''
+    KEY_FPR=$(gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10)
+    echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key "$KEY_FPR" trust quit
     echo "use-agent" >> ~/.gnupg/gpg.conf
     echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
     gpgconf --kill gpg-agent
     export GPG_TTY=$(tty)
-    """
+    '''
+
 
     // sh "curl -v -u \"$NEXUS_USER:$NEXUS_PASS\" --upload-file ~/rpmbuild/RPMS/x86_64/my-package.rpm \"$NEXUS_URL/my-package.rpm\""
 
