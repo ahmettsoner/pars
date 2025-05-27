dfacd19130506d91c1dd1ab9ec916d379a398b9a
Your Name <you@example.com>
2025-05-27 16:58:19 +0300

    ek1

--- .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
@@ -9,7 +9,7 @@ def call(String OS, String ARCH) {
     sh "echo \"${GPG_PRIVATE_KEY_B64}\" | base64 -d > /tmp/private.gpg"
     sh "gpg --batch --import /tmp/private.gpg"
 
-    sh "KEY_FPR=$(gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10)"
+    sh "KEY_FPR=\"$(gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10)\""
     sh "echo -e \"5\ny\n\" | gpg --command-fd 0 --expert --edit-key \"$KEY_FPR\" trust"
     sh "echo \"use-agent\" >> ~/.gnupg/gpg.conf"
     sh "echo \"allow-loopback-pinentry\" >> ~/.gnupg/gpg-agent.conf"
@@ -17,8 +17,7 @@ def call(String OS, String ARCH) {
     sh "GPG_TTY=$(tty)"
     sh "export GPG_TTY"
 
-    sh "rpm --define \"%_gpg_name ${GPG_SIGN_KEY}\" --addsign ${rpmOutputPath}"
-    sh "rpm -K ${rpmOutputPath}"
+    // sh "curl -v -u \"$NEXUS_USER:$NEXUS_PASS\" --upload-file ~/rpmbuild/RPMS/x86_64/my-package.rpm \"$NEXUS_URL/my-package.rpm\""
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
 }
