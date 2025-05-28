6e233b41fb79ddef34103b7e1e38dbf0e68addb8
Your Name <you@example.com>
2025-05-28 17:19:34 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
@@ -4,16 +4,39 @@ def call(String OS, String ARCH) {
     def rpmOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
 
     // Secret file kullanımı (gpg-private-key ID'li secret file)
-    withCredentials([file(credentialsId: 'gpg-private-key', variable: 'GPG_KEY_FILE')]) {
+    withCredentials([file(credentialsId: 'public-rpm.gpg', variable: 'GPG_KEY_FILE')]) {
+        // GPG key import
         sh "gpg --batch --import ${GPG_KEY_FILE}"
+
+        // Get key fingerprint
+        def keyFpr = sh(script: "gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10", returnStdout: true).trim()
+
+        // Set trust level to ultimate (trust level 6)
+        writeFile file: 'trust.txt', text: "${keyFpr}:6:\n"
+        sh "gpg --import-ownertrust trust.txt"
+
+        // GPG agent ayarları
         sh '''
-        echo "%_signature gpg" > ~/.rpmmacros
-        echo "%_gpg_name Your Name <your.email@example.com>" >> ~/.rpmmacros
+        echo "use-agent" >> ~/.gnupg/gpg.conf
+        echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+        gpgconf --kill gpg-agent
+
+        cat > ~/.rpmmacros <<EOF
+        %_signature gpg
+        %_gpg_name Your Name <your.email@example.com>
+        EOF
+
+        export GPG_TTY=$(tty || true)
+        rpm --addsign ${rpmOutputPath}
         '''
+
+
+        // RPM paketini imzala
         sh "rpm --addsign ${rpmOutputPath}"
     }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
+    sh 'rm -rf ~/.gnupg trust.txt'
 }
 
 return this
