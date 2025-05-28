88439cff17e0f0ca21e5a064e2a371f0309ed47c
Your Name <you@example.com>
2025-05-28 16:39:48 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
@@ -2,11 +2,11 @@ def call(String OS, String ARCH) {
     unstash "${OS}-${ARCH}-rpm-package-artifacts"
     def newBaseName = "${APPNAME}-${OS}-${ARCH}.rpm"
     def rpmOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
-    def RPM_FILENAME = "${APP_NAME}-${CURRENT_BASE_VERSION_RAW}-${BUILD_VERSION_RELEASE_NUMBER}.${ARCH}.rpm"
+    def RPM_FILENAME = "${APPNAME}-${CURRENT_BASE_VERSION_RAW}-${BUILD_VERSION_RELEASE_NUMBER}.${ARCH}.rpm"
 
 
     withCredentials([usernamePassword(credentialsId: 'nexus-credential', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
-        sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${rpmOutputPath} \"${NEXUS_URL}/yum/${APPNAME}.rpm\""
+        sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${rpmOutputPath} \"${NEXUS_URL}/yum/${RPM_FILENAME}.rpm\""
     }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
