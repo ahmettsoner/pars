d8e5ca8ddabb1df5d0272becf954ace7a1e0cbf0
Your Name <you@example.com>
2025-05-28 09:07:10 +0300

    elk

--- .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishRpmPackage.groovy
@@ -3,9 +3,9 @@ def call(String OS, String ARCH) {
     def newBaseName = "${APPNAME}-${OS}-${ARCH}.rpm"
     def rpmOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
 
-
-    sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${rpmOutputPath} \"${NEXUS_URL}/${APPNAME}.rpm\""
-
+    withCredentials([usernamePassword(credentialsId: 'nexus-credential', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+        sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${rpmOutputPath} \"${NEXUS_URL}/yum/${APPNAME}.rpm\""
+    }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
 }
--- .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signRpmPackage.groovy
@@ -3,26 +3,29 @@ def call(String OS, String ARCH) {
     def newBaseName = "${APPNAME}-${OS}-${ARCH}.rpm"
     def rpmOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
 
-    // GPG key import
-    sh "echo \"${GPG_PRIVATE_KEY_B64}\" | base64 -d > /tmp/private.gpg"
-    sh "gpg --batch --import /tmp/private.gpg"
+    // Secret file kullanımı (gpg-private-key ID'li secret file)
+    withCredentials([file(credentialsId: 'gpg-private-key', variable: 'GPG_KEY_FILE')]) {
+        // GPG key import
+        sh "gpg --batch --import ${GPG_KEY_FILE}"
 
-    // Get key fingerprint
-    def keyFpr = sh(script: "gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10", returnStdout: true).trim()
+        // Get key fingerprint
+        def keyFpr = sh(script: "gpg --list-keys --with-colons | grep fpr | head -n1 | cut -d':' -f10", returnStdout: true).trim()
 
-    // Set trust level to ultimate (trust level 6)
-    writeFile file: 'trust.txt', text: "${keyFpr}:6:\n"
-    sh "gpg --import-ownertrust trust.txt"
+        // Set trust level to ultimate (trust level 6)
+        writeFile file: 'trust.txt', text: "${keyFpr}:6:\n"
+        sh "gpg --import-ownertrust trust.txt"
 
-    // GPG agent ayarları
-    sh '''
-    echo "use-agent" >> ~/.gnupg/gpg.conf
-    echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
-    gpgconf --kill gpg-agent
-    export GPG_TTY=$(tty || true)
-    '''
+        // GPG agent ayarları
+        sh '''
+        echo "use-agent" >> ~/.gnupg/gpg.conf
+        echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+        gpgconf --kill gpg-agent
+        export GPG_TTY=$(tty || true)
+        '''
+    }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-rpm-package-artifacts"
+    sh 'rm -rf ~/.gnupg trust.txt'
 }
 
 return this
