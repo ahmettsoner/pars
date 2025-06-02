ec9aed1e6ecc2e111713f26ba98baf446db8c6b7
Your Name <you@example.com>
2025-06-02 08:05:06 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/signDebPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signDebPackage.groovy
@@ -52,14 +52,14 @@ EOF
             echo "Signing file: ${debOutputPath}"
             file "${debOutputPath}"
             export DEBSIG_GPG_EXECUTABLE=./gpg-wrapper
-            dpkg-sig --sign builder -k "$GPG_FINGERPRINT" "${debOutputPath}"
+            // dpkg-sig --sign builder -k "$GPG_FINGERPRINT" "${debOutputPath}"
+            echo "$GPG_PASSPHRASE" | dpkg-sig --sign builder -k "$GPG_FINGERPRINT" -- --pinentry-mode loopback "${debOutputPath}"
         """
 
 
-        // echo "$GPG_PASSPHRASE" | dpkg-sig --sign builder -k "$GPG_FINGERPRINT" -- --pinentry-mode loopback "${debOutputPath}"
         // sh "echo \"$GPG_PASSPHRASE\" | dpkg-sig --sign builder -k \"$GPG_FINGERPRINT\" \"$debOutputPath\""
 
         
-        sh 'rm -rf ~/.gnupg trust.txt fpr.txt gpg-wrapper'
+        sh 'rm -rf ~/.gnupg trust.txt fpr.txt pass.txt gpg-wrapper'
     }
 }
