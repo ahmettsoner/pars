18de47afb75079d5486968c26da4977e95e70354
Your Name <you@example.com>
2025-06-02 18:17:32 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/publishDebPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishDebPackage.groovy
@@ -36,7 +36,7 @@ def call(String OS, String ARCH, String DIST_CODENAME) {
             curl -v -u "${NEXUS_USER}:${NEXUS_PASS}" \\
                 -H "Content-Type: multipart/form-data" \\
                 --data-binary "@${debOutputPath}" \\
-                "${NEXUS_URL}/repository/apt-${DIST_CODENAME}/"
+                "${NEXUS_URL}/apt-${DIST_CODENAME}/"
         """
 
     }
