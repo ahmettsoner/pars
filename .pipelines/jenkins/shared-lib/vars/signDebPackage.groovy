6277bdd6d927758c9fcb4c7c15843673e480cf2f
Your Name <you@example.com>
2025-06-02 11:03:17 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/signDebPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/signDebPackage.groovy
@@ -1,4 +1,4 @@
-def call(String OS, String ARCH, String DIST_CODENAME){
+def call(String OS, String ARCH, List<String> DIST_CODENAMES){
     withCredentials([
         file(credentialsId: 'public-rpm.gpg', variable: 'GPG_PUBLIC'),
         file(credentialsId: 'private-rpm.gpg', variable: 'GPG_PRIVATE'),
@@ -11,7 +11,9 @@ def call(String OS, String ARCH, String DIST_CODENAME){
         def newBaseName = "${APPNAME}-${OS}-${ARCH}.deb"
         def debOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
         def aptlyRepoName = "${APPNAME}-${OS}-${ARCH}"
-        def publishDir = "${env.WORKSPACE}/aptly-publish"
+
+        def packageOutputBase = "dist/${env.BUILD_VERSION}/${OS}/pkg/deb"
+        def publishDir = "${packageOutputBase}/publish"
 
         sh '''
             set -e
@@ -32,37 +34,32 @@ def call(String OS, String ARCH, String DIST_CODENAME){
             gpgconf --kill gpg-agent || true
             gpgconf --launch gpg-agent || true
         '''
-        sh """
-            echo '>>>>'
-            ar t ${debOutputPath}
-        """
 
-        // Create aptly repo and publish
+        sh "ar t ${debOutputPath}"
+
         sh """
-            # Clean any existing repo with the same name
             aptly repo drop -force ${aptlyRepoName} || true
-
-            # Create a new local repo
-            aptly repo create -distribution=${DIST_CODENAME} -component=main ${aptlyRepoName}
+            aptly repo create -component=main ${aptlyRepoName}
             aptly repo add ${aptlyRepoName} ${debOutputPath}
-
-            # Publish the repo with GPG signing
-            aptly publish drop ${DIST_CODENAME} || true
-
-            echo "$GPG_PASSPHRASE" | aptly publish repo \
-                -distribution=${DIST_CODENAME} \
-                -passphrase="$GPG_PASSPHRASE" \
-                -architectures=${ARCH} \
-                ${aptlyRepoName}
         """
 
-        // Optionally archive output for upload to Nexus
+        DIST_CODENAMES.each { dist ->
+            sh """
+                aptly publish drop ${dist} || true
+
+                echo "$GPG_PASSPHRASE" | aptly publish repo \
+                    -distribution=${dist} \
+                    -passphrase="$GPG_PASSPHRASE" \
+                    -architectures=${ARCH} \
+                    ${aptlyRepoName}
+            """
+        }
+
         sh """
             mkdir -p ${publishDir}
             cp -r ~/.aptly/public/* ${publishDir}/
         """
 
-        // Cleanup
         sh 'rm -rf ~/.gnupg trust.txt ~/.aptly'
 
         stash name: "${OS}-${ARCH}-aptly-repo", includes: 'aptly-publish/**/*'
