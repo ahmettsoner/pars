2421c124d4934a56f6c3b4649fe4e5cc91cfa82b
Your Name <you@example.com>
2025-06-02 11:52:42 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/publishDebPackage.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishDebPackage.groovy
@@ -4,9 +4,16 @@ def call(String OS, String ARCH, String DIST_CODENAME) {
     def debOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
     def DEB_FILENAME = "${APPNAME}-${CURRENT_BASE_VERSION_RAW}-${BUILD_VERSION_RELEASE_NUMBER}.${ARCH}.deb"
 
-
+    // Nexus APT upload path:
+    def firstChar = APPNAME[0].toLowerCase()
+    def uploadUrl = "${NEXUS_URL}/apt-${DIST_CODENAME}/pool/main/${firstChar}/${APPNAME}/${DEB_FILENAME}"
+    
+    echo "Uploading DEB package: ${debOutputPath}"
+    echo "Upload URL: ${uploadUrl}"
+    
     withCredentials([usernamePassword(credentialsId: 'nexus-credential', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
         sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${debOutputPath} \"${NEXUS_URL}/apt-${DIST_CODENAME}/${DEB_FILENAME}\""
+        sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${debOutputPath} \"${uploadUrl}\""
     }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-deb-package-artifacts"
