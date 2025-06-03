d8947ed5e8d162675f9cc87298f056c4e88c739c
Your Name <you@example.com>
2025-06-03 11:48:44 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/releaseManagement.groovy
+++ .pipelines/jenkins/shared-lib/vars/releaseManagement.groovy
@@ -138,7 +138,7 @@ def releaseRepo() {
                     def newBaseName = "${APPNAME}-${OS}-${ARCH}${ext}"
                     def remoteFilePath = "${NEXUS_URL}/repository/raw/downloads/${APPNAME}/${CHANNEL}/${env.BUILD_VERSION}/${OS}/${ARCH}/${ARCH}${ext}"
 
-                    sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${filePath} \"${remoteFilePath}\""
+                    sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file ${newBaseName} \"${remoteFilePath}\""
 
 
                     if (OS == 'linux') {
