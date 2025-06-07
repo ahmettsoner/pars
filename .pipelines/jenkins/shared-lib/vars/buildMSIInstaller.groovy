a416e7a7efd609e133248f54e865922a7c07ab0d
Your Name <you@example.com>
2025-06-07 12:48:42 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/buildMSIInstaller.groovy
+++ .pipelines/jenkins/shared-lib/vars/buildMSIInstaller.groovy
@@ -11,26 +11,26 @@ def call(String OS, String ARCH) {
     def packageSourceDir = "${packageOutputBase}\\SOURCES"
 
     // Create zip (Windows PowerShell)
-    powershell """
+    powershell '''
         \$ErrorActionPreference = 'Stop'
-        New-Item -ItemType Directory -Force -Path '\${packageSourceDir}' | Out-Null
-        Push-Location '\${binaryOutputBase}'
+        New-Item -ItemType Directory -Force -Path '${packageSourceDir}' | Out-Null
+        Push-Location '${binaryOutputBase}'
         try {
-            Compress-Archive -Path * -DestinationPath '\${newBaseName}' -Force
+            Compress-Archive -Path * -DestinationPath '${newBaseName}' -Force
         } catch {
             Write-Host "Compress-Archive failed: $($_.Exception.Message)"
         }
-        if (!(Test-Path '\${newBaseName}')) {
+        if (!(Test-Path '${newBaseName}')) {
             Write-Host "Error with archive: Unable to validate archive contents."
         }
-        Copy-Item -Path '\${newBaseName}' -Destination '\${WORKSPACE}\\\${packageSourceDir}' -Force
+        Copy-Item -Path '${newBaseName}' -Destination '${WORKSPACE}\\${packageSourceDir}' -Force
         Pop-Location
-    """
+    '''
 
     // Build MSI package
-    powershell """
+    powershell '''
         \$env:GO111MODULE = "on"
-        make build.msi.package.\${ARCH}.configuration VERSION=\${env.CURRENT_VERSION}
-        make build.msi.package.\${ARCH}.package VERSION=\${env.CURRENT_VERSION}
-    """
+        make build.msi.package.${ARCH}.configuration VERSION=${env.CURRENT_VERSION}
+        make build.msi.package.${ARCH}.package VERSION=${env.CURRENT_VERSION}
+    '''
 }
