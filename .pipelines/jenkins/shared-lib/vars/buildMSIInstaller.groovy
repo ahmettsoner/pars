931cd5e1e86083064586228d3eb28221af51ef77
Your Name <you@example.com>
2025-06-07 15:27:26 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/buildMSIInstaller.groovy
+++ .pipelines/jenkins/shared-lib/vars/buildMSIInstaller.groovy
@@ -11,11 +11,10 @@ def call(String OS, String ARCH) {
     def packageSourceDir = "${packageOutputBase}\\SOURCES"
 
     powershell """
-            Write-Host ">>>>>: ${packageSourceDir}'"
+        Write-Host ">>>>>: ${packageSourceDir}'"
     """
 
-    // Create zip (Windows PowerShell)
-    powershell """
+    powershell '''
         \$ErrorActionPreference = 'Stop'
         New-Item -ItemType Directory -Force -Path '${packageSourceDir}' | Out-Null
         Push-Location '${binaryOutputBase}'
@@ -29,7 +28,7 @@ def call(String OS, String ARCH) {
         }
         Copy-Item -Path '${newBaseName}' -Destination '${WORKSPACE}\\${packageSourceDir}' -Force
         Pop-Location
-    """
+    '''
 
     // Build MSI package
     powershell """
