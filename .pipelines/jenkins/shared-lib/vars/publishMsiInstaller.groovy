6c2b2a9fe83260fdd118b87cb5a686b25a3376be
Your Name <you@example.com>
2025-06-22 01:31:33 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/publishMsiInstaller.groovy
+++ .pipelines/jenkins/shared-lib/vars/publishMsiInstaller.groovy
@@ -1,17 +1,14 @@
 def call(String OS, String ARCH) {
     unstash "${OS}-${ARCH}-msi-package-artifacts"
     def newBaseName = "${APPNAME}-${OS}-${ARCH}.msi"
-    def msiOutputPath = "dist\\artifacts\\${env.CURRENT_VERSION}\\${newBaseName}"
+    def msiOutputPath = "${env.ARTIFACT_PATH}/${newBaseName}"
+
+    def utils = new com.parsdevkit.Utils(this)
+    def platformArch = utils.mapArch("windows", ARCH)
+    def MSI_FILENAME = "${APPNAME}-${CURRENT_VERSION}.${platformArch}.msi"
 
     withCredentials([usernamePassword(credentialsId: 'nexus-credential', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
-        powershell """
-            \$ProgressPreference = 'SilentlyContinue'
-            Invoke-RestMethod -Uri "${NEXUS_URL}/repository/apt-${CHANNEL}/" `
-                              -Method Post `
-                              -Headers @{"Content-Type" = "multipart/form-data"} `
-                              -InFile "${msiOutputPath}" `
-                              -Credential (New-Object System.Management.Automation.PSCredential('${NEXUS_USER}', (ConvertTo-SecureString '${NEXUS_PASS}' -AsPlainText -Force)))
-        """
+        sh "curl -v -u \"${NEXUS_USER}:${NEXUS_PASS}\" --upload-file \"${msiOutputPath}\" \"${NEXUS_URL}/repository/msi-${CHANNEL}/${MSI_FILENAME}\""
     }
 
     stash includes: 'dist/artifacts/**/*', name: "${OS}-${ARCH}-msi-package-artifacts"
