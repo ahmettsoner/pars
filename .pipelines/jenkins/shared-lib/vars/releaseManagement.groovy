eb71bad0dcc8151a4cd9c19da18b69d3b63e846e
Your Name <you@example.com>
2025-06-22 00:32:33 +0300

    ek

--- .pipelines/jenkins/shared-lib/vars/releaseManagement.groovy
+++ .pipelines/jenkins/shared-lib/vars/releaseManagement.groovy
@@ -97,7 +97,7 @@ def releaseRepo(List<String> osList, List<String> archList) {
 
                         def newMsiBaseName = "${APPNAME}-${OS}-${ARCH}.msi"
                         def remoteMsiFilePath = "${NEXUS_URL}/repository/raw-dev/${APPNAME}/${CHANNEL}/${env.CURRENT_VERSION}/${OS}/${platformArch}/${APPNAME}.msi"
-                        def localMsiPath = "${env.ARTIFACT_PATH}\\${newmsiBaseName}"
+                        def localMsiPath = "${env.ARTIFACT_PATH}/${newmsiBaseName}"
 
                         withCredentials([usernamePassword(credentialsId: 'nexus-credential', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
                             powershell """
